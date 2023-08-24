52bada6d6ed0bd4e041c88dc17103a711305fd44
liulu1550 <wouldmissyou@163.com>
2023-08-24 18:03:11 +0800

    redis视图

--- applications/apps/demo2/router.py
+++ applications/apps/demo2/router.py
@@ -1,8 +1,11 @@
 from sanic import Blueprint
 
-from applications.apps.demo2.views import demo2_view
+from applications.apps.demo2.views import demo2_view, get_redis_value, set_redis_value
 
 router: Blueprint = Blueprint('demo2', url_prefix='demo2')
 
 
 router.add_route(demo2_view, "/demo2", ('GET',))
+
+router.add_route(get_redis_value, "/redis", ('GET',))
+router.add_route(set_redis_value, "/set_redis", ('GET',))
--- applications/apps/demo2/views.py
+++ applications/apps/demo2/views.py
@@ -1,6 +1,11 @@
+from typing import Any
+
+from aioredis import Redis
 from sanic import Request, HTTPResponse
 
 from core.response import response_ok
+from utils import redis_utils
+from utils.redis_utils import get_value, set_value
 
 
 async def demo2_view(request: Request) -> HTTPResponse:
@@ -9,4 +14,18 @@ async def demo2_view(request: Request) -> HTTPResponse:
     :param request:
     :return:
     """
+
+    return response_ok()
+
+
+async def get_redis_value(request):
+    data = await get_value(request, "13")
+    print(data)
+    return response_ok()
+
+
+
+
+async def set_redis_value(request):
+    data = await set_value(request, "cheshi", 123, 20)
     return response_ok()
